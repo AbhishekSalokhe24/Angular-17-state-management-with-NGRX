7814c741627d34f156e64b740029639a82122b37
AbhishekSalokhe24 <abhisaloke2424@gmail.com>
2024-05-31 11:45:46 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MyCounterComponent } from "./my-counter/my-counter.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, MyCounterComponent]
 })
 export class AppComponent {
   title = 'counter-redux';
